ea23229fd943863a39672e193fe10acc5298b829
estherfernandezrincon <73235763+estherfernandezrincon@users.noreply.github.com>
2021-03-17 16:01:03 +0100

    funciona validar

--- MYLOGIN/login.py
+++ MYLOGIN/login.py
@@ -2,38 +2,35 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
-import requests
+
 from sqlite3 import Error
 from configparser import ConfigParser
 
-
-
-#DBFILE= config ['DBFILE']
+c= ConfigParser()
+c.read('MYLOGIN/config.ini')
+config = c ['DEFAULT']
+DBFILE = config ['DBFILE']
 
 
 class Login(ttk.Frame):
     def __init__(self, parent):
-        ttk.Frame.__init__(self, parent, width=500, height=700)
-        self.pack_propagate(False)
-
-
-        self.Forgot = ttk.Button(self, text="Forgot my password" )
-        self.Forgot.pack(side= BOTTOM, fill=BOTH, expand=True, padx=30, pady=30)        
+        ttk.Frame.__init__(self, parent, width=700, height=500)
+        self.pack_propagate(False)       
         
         self.label_usuario= ttk.Label(self,width=20, text="user:",font="Times")
         self.label_usuario.pack(side=TOP, fill=BOTH, expand = True, padx=5, pady=5)
-        self.user=StringVar()
+        self.usuario=StringVar()
         
-        self.entry_usuario=ttk.Entry (self,textvariable=self.user, foreground="red", font="Times,10", width=20, state="disable")        
+        self.entry_usuario=ttk.Entry (self,textvariable=self.usuario, foreground="red", font="Times,10", width=20, state="disable")        
         self.entry_usuario.pack(side=TOP, fill= X, expand=True, padx=25, pady=25)
         self.separar=ttk.Separator(self, orient=HORIZONTAL)
         self.separar.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=5)
 
         self.label_contraseña= ttk.Label(self, width=20, text="password:",font="Times")
         self.label_contraseña.pack(side=TOP, fill=BOTH, expand = True, padx=5, pady=5)
-        self.password=StringVar()  
+        self.contraseña=StringVar()  
 
-        self.entry_contraseña=ttk.Entry(self, textvariable=self.password,foreground="blue",font= "Times, 10", width=30, show='*', state="disabled")
+        self.entry_contraseña=ttk.Entry(self, textvariable=self.contraseña, foreground="blue",font= "Times, 10", width=30, show='*', state="disabled")
         self.entry_contraseña.pack(side=TOP, fill= X, expand=True, padx=25, pady=25)
         self.separar=ttk.Separator(self, orient=HORIZONTAL)
         self.separar.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=5)
@@ -44,8 +41,6 @@ class Login(ttk.Frame):
         self.YaEstoyRegistrado= ttk.Button(self, text="Already registered", command=self.AlreadyRegistered)
         self.YaEstoyRegistrado.pack(side=TOP, fill= BOTH, expand= True, padx=15, pady=15) 
 
-        self.Robot= ttk.Checkbutton(self, text="I'm not a robot")
-        self.Robot.pack(side=TOP, fill=BOTH, expand=False, padx=30, pady=30)
         
         self.botonA = ttk.Button(self, text="Accept",command=self.Validar)
         self.botonA.pack(side=LEFT, fill=BOTH, expand=True, padx=35, pady=35)
@@ -58,33 +53,60 @@ class Login(ttk.Frame):
 
         
     def Validar(self):        
-        usuario = self.user.get()
-        contraseña = self.password.get()
+        usuario = self.usuario.get()
+        contraseña = self.contraseña.get()
+      
 
         conn = sqlite3.connect("MYLOGIN/data/base_de_datos.db")
         c = conn.cursor()
 
-        c.execute( 'SELECT Name, password from REGISTRO;')
+        c.execute( 'SELECT Name, Password FROM REGISTRO;')
         datos_introducidos = c.fetchall()
+  
+        conn.commit()
+        conn.close()
+   
 
-        if usuario in datos_introducidos[0] and contraseña in datos_introducidos[0]:           
-            label= ttk.Label( text="access successful ", background="light grey",foreground="red", font="Times, 13", anchor=S)
+        a=[]
+        b=[]
+        
+        for usuario in datos_introducidos:
+            if usuario in datos_introducidos :
+                a.append(usuario[0])             
+
+
+        for contraseña in datos_introducidos:
+            if contraseña in datos_introducidos:
+                b.append(contraseña[1])
+
+      
+ 
+
+    
+        if self.usuario.get() in a and self.contraseña.get() in b: 
+                
+            label= ttk.Label( text="acceso correcto ", background="light grey",foreground="red", font="Times, 13", anchor=S)
             label.pack(side=BOTTOM, fill= BOTH, expand= True)
 
-        elif usuario in datos_introducidos[0] or contraseña in datos_introducidos[0]:
-            label= ttk.Label( text="usuario o contraseña incorrectos ", background="light grey",foreground="red", font="Times, 13", anchor=S)
-            label.pack(side=BOTTOM, fill= BOTH, expand= True)    
+
+        elif self.usuario.get()in a or self.contraseña.get() in b:
+            label= ttk.Label( text="usuario o contraseña incorrectos", background="light grey",foreground="red", font="Times, 13", anchor=S)
+            label.pack(side=BOTTOM, fill= BOTH, expand= True)
 
         else:
-            label= ttk.Label( text="¿¿estás registrado??", background="light grey",foreground="red", font="Times, 13", anchor=S)
+            label= ttk.Label( text="Debes registrarte primero", background="light grey",foreground="red", font="Times, 13", anchor=S)
             label.pack(side=BOTTOM, fill= BOTH, expand= True)
-
-
+                
+        if self.usuario != "" and self.contraseña != "":
+            self.usuario.set("")
+            self.contraseña.set("")
+        
         
     def Register(self):
         newWindow= Toplevel()
         newWindow.geometry("350x450")
         newWindow.grab_set()
+ 
         
         self.label_nuevo= ttk.Label(newWindow, text= "New Register")
         self.label_nuevo.pack()
@@ -100,16 +122,33 @@ class Login(ttk.Frame):
         self.lbl_pass.pack(side=TOP, fill=BOTH, expand = True, ipadx=1, ipady=1)
         self.newpass=StringVar() 
 
-        self.entry_passw=ttk.Entry (newWindow,textvariable=self.newpass , foreground="red", font="Times,10", width=5, show='*')        
+        self.entry_passw=ttk.Entry (newWindow, textvariable=self.newpass , foreground="red", font="Times,10", width=5, show='*')        
         self.entry_passw.pack(side=TOP, fill= X, expand=True, padx=25, pady=25)
         
         self.button_nuevo=ttk.Button(newWindow, text= "Create Account", command= self.Datos)
         self.button_nuevo.pack(side=TOP)
 
+        
+
 
     def Datos(self):
+
+        okRegistro= Toplevel()
+        okRegistro.geometry("450x150")
+        okRegistro.grab_set()
+
         NewUser = self.newuser.get()
         NewPass = self.newpass.get()
+
+        longitud = len(NewPass)
+        upper = NewPass.upper()
+      
+        if longitud < 8 and NewPass != upper:
+            self.label= ttk.Label(okRegistro, text="La contraseña debe contener al menos 8 caracteres y una mayuscula ", background="light grey",foreground="red", font="Times, 13", anchor=CENTER)
+            self.label.pack(side=BOTTOM, fill= BOTH, expand= True)                   
+        else:
+            self.label= ttk.Label(okRegistro,  text="registro correcto ", background="light grey",foreground="green", font="Times, 13", anchor=CENTER)
+            self.label.pack(side=BOTTOM, fill= BOTH, expand= True)
 
         conn = sqlite3.connect("MYLOGIN/data/base_de_datos.db")
         c = conn.cursor()
@@ -117,9 +156,24 @@ class Login(ttk.Frame):
         c.execute('INSERT INTO REGISTRO ( Name, Password ) VALUES (?,?) ;' , 
                 (NewUser, NewPass))
 
+        print(NewUser)
+
         conn.commit()
         conn.close()
-    
+        
+
+        if self.newuser != "" and self.newpass != "":
+            self.newuser.set("")
+            self.newpass.set("")
+
+        
+               
+            
+
+            
+
+        
+         
         
 
         
---
+++ readme.md
@@ -0,0 +1,14 @@
+# CREAR ARCHIVO PARA PROTEGER RUTA BASE DE DATOS 
+
+Creamos un fichero dentro de MYLOGIN con nombre config.ini. El fichero comienza con [DEFAULT]
+En él creamos una variable con nuestra ruta a base de datos.
+Se debe escribir sin comillas ni espacios.
+Ver archivo config_template.ini como referencia.
+
+# MANEJO DE LA APLICACIÓN
+
+Una vez lanzada la apliación, deberás hacer click en New Register para crear tu usuario y contraseña.
+
+Cuando esté correctamente creada, en la ventana principal hacer click en Already Registered.
+Se habilitarán los campos para introducir el usuario y contraseña creados previamente.
+Dar a aceptar para ver si es correcto el acceso, clicar en Cancelar para salir de la aplicación.
